6df8c1dceca5810077daa1bfe0fb464e128d60a7
John Tuttle <thejwt@gmail.com>
2017-09-08 21:01:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,20 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var uppercase = "YES INDEED!"
-  var lowercase = "hello"
-  var mixedCase = "I love you, Grandma."
+  var uppercase = "HELLO!"
+ 
+  uppercase.toUpperCase() === uppercase // true
+ 
+  var lowercase = 'hello!'
+ 
+  lowercase.toLowerCase() === lowercase // true
+ 
+  var mixedCase = 'Hi there!'
+ 
+  mixedCase.toLowerCase() === mixedCase // false
+ 
+  mixedCase.toUpperCase() === mixedCase // false
+  
   if (uppercase){
     return "YES INDEED!"
   } else if (lowercase){
