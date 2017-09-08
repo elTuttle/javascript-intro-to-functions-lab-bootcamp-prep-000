c998cc20a580d9cadff7874226a064d97de1dfc1
John Tuttle <thejwt@gmail.com>
2017-09-08 20:45:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  return console.log(string.toUpperCase)
+}
+
+function logWhisper(string){
+  return console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string){
+  if()
+}
